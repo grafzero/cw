d1db921eb464e121cef6b795e15bb025f25cd690
maciek <maciekslotwinski@gmail.com>
2017-11-10 21:17:43 +0100

    init

--- js/3dText.js
+++ js/3dText.js
@@ -1,10 +1,11 @@
 // effect from: https://codemyui.com/3d-hover-card/
+// translated to vanilla js
 
 
 
 var heading = document.getElementById('headerText');
 window.onmousemove = function(e){
-   // console.log('ruszam myszko!');
+
     var ax = -(window.innerWidth / 2 - e.pageX)/20;
     var ay = (window.innerHeight/2 - e.pageY)/10;
     heading.style.webkitTransform = "rotateY(\"+ax+\"deg) rotateX(\"+ay+\"deg)";
